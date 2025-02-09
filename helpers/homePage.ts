51e88a9c6f62f02e297ba2d3e6c2278aa09ac163
mikekruithof22@hotmail.com <mikekruithof22@hotmail.com>
2025-02-09 17:34:01 +0100

    smoke test 2 (almost done) and 3 (under construction)

--- helpers/homePage.ts
+++ helpers/homePage.ts
@@ -3,14 +3,19 @@ import { Page, expect } from "@playwright/test";
 export class HomePageHelper {
     baseUrl: string = 'https://www.funda.nl/';
 
-    async loadBaseUrl(page: Page) {
+    async loadPageAndAcceptCookies(page: Page) {
+        await this.loadBaseUrl(page);
+        await this.acceptAllCookies(page);
+    }
+
+    private async loadBaseUrl(page: Page) {
         await page.goto(this.baseUrl);
     }
     
-    async acceptAllCookies(page: Page) {
+    private async acceptAllCookies(page: Page) {
         await expect(page.getByTestId("notice")).toBeVisible();
         await page
             .getByRole("button", { name: "Akkoord  en sluiten: Akkoord" })
             .click();
-    }
+    }  
 }
--- tests/smoke-tests.spec.ts
+++ tests/smoke-tests.spec.ts
@@ -9,8 +9,7 @@ test.use({ userAgent: process.env.USER_AGENT });
 test("critical elements home page test", async ({ page }) => {
 
   const homePageHelper = new HomePageHelper();
-  await homePageHelper.loadBaseUrl(page);
-  await homePageHelper.acceptAllCookies(page);
+await homePageHelper.loadPageAndAcceptCookies(page);
 
   // Three most crucial HTML elements
   await expect(
@@ -36,26 +35,80 @@ test("critical elements home page test", async ({ page }) => {
   await expect(page.getByRole("link", { name: "English" })).toBeVisible();
 });
 
-test("search results for buyers sanity checks", async ({ page }) => {
-    
-    
-    /*
-       page.getByTestId("search-box") 
+test("search results for buyers appear", async ({ page }) => {
+    const homePageHelper = new HomePageHelper();
+    await homePageHelper.loadPageAndAcceptCookies(page);
 
-    */
+    await page.getByTestId('search-box').click();
+    await page.getByTestId('search-box').fill('Amsterdam');
+
+    new Promise(resolve => setTimeout(resolve, 2000)); // TODO: tmp code, remove when doe. 
+    await page.getByRole('option', { name: 'Amsterdam Plaats in Noord-' }).click();
+
+    // check if search query is inside the search container and some filters
+    await expect(page.getByTestId('searchBoxSuggestions-mobile').getByText('Amsterdam')).toBeVisible();
+
+
+    await page.waitForSelector('div.border-b.pb-3');
+
+    const searchResultContainers = page.locator('div.border-b.pb-3');
+    const count = await searchResultContainers.count();
+
+    // Only check the first three search results for image and check for data-testid="listingDetailsAddress"
+    for (let i = 0; i < 4; i++) {
+        const searchResultContainer = searchResultContainers.nth(i);
+        await expect(searchResultContainer).toBeVisible();
+        const listingDetailsAddress = searchResultContainer.locator('[data-testid="listingDetailsAddress"]');
+        await expect(listingDetailsAddress).toBeVisible();
+    }
 });
 
-test("user login sanity checks", async ({ page }) => {
+// TODO: third test is work in progress
+test("check search results amount after filtering once", async ({ page }) => {
+    const homePageHelper = new HomePageHelper();
+    await homePageHelper.loadPageAndAcceptCookies(page);
+
+    await page.getByTestId('search-box').click();
+    await page.getByTestId('search-box').fill('Amsterdam');
+    await page.getByRole('option', { name: 'Amsterdam Plaats in Noord-' }).click();
+
+    const amountOfHousesCount = page.locator('div.truncate.font-semibold');
+
+    // await expect(page.getByRole('button', { name: 'Bewaar zoekopdracht' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Filters' })).toBeVisible();
+
+
+    // filter op 'Woonhuis' and check the url and filter "woonhuis" filter is visible()
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await expect(page.getByText('FiltersSluitenKoopHuurPrijs')).toBeVisible();
+
+    await page.getByRole('checkbox', { name: 'Woonhuis' }).check();
+    expect(page.getByRole('checkbox', { name: 'Woonhuis' })).toBeChecked();
+
+    await expect(page).toHaveURL(`${homePageHelper}?object_type=["house"]`);
+    const woonhuisFilter = page.locator('li[data-testid="SelectedFilterobject_type"]');
+    await expect(woonhuisFilter).toBeVisible();
+
+
 
 });
 
-test("makelaar login sanity checks", async ({ page }) => {
+// test("home search results filter checks", async ({ page }) => {
 
-});
+// });
 
-test("...", async ({ page }) => {
 
-});
+// test("user login sanity checks", async ({ page }) => {
+
+// });
+
+// test("makelaar login sanity checks", async ({ page }) => {
+
+// });
+
+// test("...", async ({ page }) => {
+
+// });
 
 
 /*
